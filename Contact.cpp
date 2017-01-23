faca36c4ceb1cb5e7152d8850b48ce27e8c6ac37
VScoders <gary-a-smith@hotmail.com>
2017-01-23 12:05:56 -0400

    Added implementation file

---
+++ Contact.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include "Contact.h"
+
+using namespace std;
+
+Contact::Contact(){
+	contactName = "";
+	phoneNum = 0;
+}
+
+string Contact::getContactName(){
+	return contactName;
+}
+
+void Contact::setContactName(string name){
+	contactName = name;
+}
+
+string Contact::getPhoneNumber(){
+	return phoneNum;
+}
+
+void Contact::setPhoneNumber(int number){
+	phoneNum = number;
+}
